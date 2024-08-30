e44b821ded3c6b43dfb692c6051ca2bb28e0cb11
migruiz <mig.ruiz@gmail.com>
2024-08-30 09:40:51 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 input.onButtonPressed(Button.A, function () {
+    movingWheel = true
     Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 45)
 })
 function init () {
@@ -11,28 +12,37 @@ function init () {
     strip = neopixel.create(DigitalPin.P15, 80, NeoPixelMode.RGB)
 }
 input.onButtonPressed(Button.B, function () {
+    movingWheel = false
     Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor1)
 })
 let ticklingBot:servoBox.TicklingBot
 let scorpioBot: servoBox.ScorpioBot
 let rabbitBot: servoBox.RabbitBot
 let strip: neopixel.Strip
+
+let movingWheel = false
+
 init()
 
 
 let showingRainbow = false
+let smallRainbowMoving = false
+let smallRainbowLocation = 0
+let smallRainbowDirection = 1
 
 let brightness = 255
 let direction = -1
 basic.forever(function () {
     if (scorpioBot.touchingStinger){
         showingRainbow = false
+        smallRainbowMoving = false
         strip.setBrightness(255)
         strip.showColor(NeoPixelColors.Red)
         basic.pause(4000)
     }
     else if (ticklingBot.tickling){
         showingRainbow = false
+        smallRainbowMoving = false
         strip.setBrightness(255)
         strip.showColor(NeoPixelColors.Blue)
         basic.pause(150)
@@ -41,6 +51,7 @@ basic.forever(function () {
     }
     else if (rabbitBot.eating) {
         showingRainbow = false
+        smallRainbowMoving = false
         strip.showColor(16731392)
         basic.pause(25)
         strip.setBrightness(brightness)
@@ -54,7 +65,27 @@ basic.forever(function () {
             direction = -1
         }
     }
+    else if (movingWheel){
+        showingRainbow = false
+        if (!(smallRainbowMoving)) {
+            smallRainbowLocation = 0
+            strip.clear()
+            const range = strip.range(0, 10)
+            range.showRainbow(1, 360)
+            smallRainbowMoving = true
+        } else {
+            if (smallRainbowLocation <= 0) {
+                smallRainbowDirection = 1
+            } else if (smallRainbowLocation > 80 - 10 - 1) {
+                smallRainbowDirection = -1
+            }
+            smallRainbowLocation = smallRainbowLocation + smallRainbowDirection
+            strip.rotate(smallRainbowDirection)
+        }
+        strip.show()
+    }
     else{
+        smallRainbowMoving = false
         strip.setBrightness(255)
         if (!showingRainbow) {
             strip.showRainbow(1, 360)
@@ -63,7 +94,7 @@ basic.forever(function () {
         else {
             strip.rotate(4)
             strip.show()
-            basic.pause(70)
+            basic.pause(100)
         }
     }
     
