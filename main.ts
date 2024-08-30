91fec89dba930df04dd4a260faabea7951c4e33e
migruiz <mig.ruiz@gmail.com>
2024-08-30 12:44:08 +0100

    refactor variables

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 input.onButtonPressed(Button.A, function () {
-    movingWheel = true
-    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 45)
+    movingWheel = !movingWheel
+    if (movingWheel){
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 45)
+    }
+    else{
+        Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor1)
+    }
 })
 function init () {
     ticklingBot = new servoBox.TicklingBot();
@@ -12,22 +17,23 @@ rabbitBot.init()
 strip = neopixel.create(DigitalPin.P15, 80, NeoPixelMode.RGB)
 }
 input.onButtonPressed(Button.B, function () {
-    movingWheel = false
-    Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor1)
 })
 let range: neopixel.Strip = null
-let smallRainbowLocation = 0
-let smallRainbowMoving = false
-let showingRainbow = false
 let movingWheel = false
+
+let smallRainbowLocation = 0
+let smallRainbowDirection = 1
+let showingSmallRainbowMoving = false
+let showingFullRainbow = false
+
 let ticklingBot:servoBox.TicklingBot
 let scorpioBot: servoBox.ScorpioBot
 let rabbitBot: servoBox.RabbitBot
 let strip: neopixel.Strip
 init()
-let smallRainbowDirection = 1
-let brightness = 255
-let direction = -1
+
+let rabbitLightsbrightness = 255
+let rabbitLightsDirection = -1
 basic.forever(function () {
     if (rabbitBot.eating) {
         basic.showIcon(IconNames.Happy)
@@ -45,41 +51,41 @@ basic.forever(function () {
 })
 basic.forever(function () {
     if (scorpioBot.touchingStinger) {
-        showingRainbow = false
-        smallRainbowMoving = false
+        showingFullRainbow = false
+        showingSmallRainbowMoving = false
         strip.setBrightness(255)
         strip.showColor(NeoPixelColors.Red)
     } else if (ticklingBot.tickling) {
-        showingRainbow = false
-        smallRainbowMoving = false
+        showingFullRainbow = false
+        showingSmallRainbowMoving = false
         strip.setBrightness(255)
         strip.showColor(NeoPixelColors.Blue)
         basic.pause(150)
         strip.showColor(NeoPixelColors.Green)
         basic.pause(150)
     } else if (rabbitBot.eating) {
-        showingRainbow = false
-        smallRainbowMoving = false
+        showingFullRainbow = false
+        showingSmallRainbowMoving = false
         strip.showColor(16731392)
         basic.pause(25)
-        strip.setBrightness(brightness)
-        brightness = brightness + direction * 8
-        if (brightness < 0) {
-            brightness = 0
-            direction = 1
+        strip.setBrightness(rabbitLightsbrightness)
+        rabbitLightsbrightness = rabbitLightsbrightness + rabbitLightsDirection * 8
+        if (rabbitLightsbrightness < 0) {
+            rabbitLightsbrightness = 0
+            rabbitLightsDirection = 1
         }
-        if (brightness > 255) {
-            brightness = 255
-            direction = -1
+        if (rabbitLightsbrightness > 255) {
+            rabbitLightsbrightness = 255
+            rabbitLightsDirection = -1
         }
     } else if (movingWheel) {
-        showingRainbow = false
-        if (!(smallRainbowMoving)) {
+        showingFullRainbow = false
+        if (!(showingSmallRainbowMoving)) {
             smallRainbowLocation = 0
             strip.clear()
             range = strip.range(0, 10)
             range.showRainbow(1, 360)
-            smallRainbowMoving = true
+            showingSmallRainbowMoving = true
         } else {
             if (smallRainbowLocation <= 0) {
                 smallRainbowDirection = 1
@@ -91,11 +97,11 @@ basic.forever(function () {
         }
         strip.show()
     } else {
-        smallRainbowMoving = false
+        showingSmallRainbowMoving = false
         strip.setBrightness(255)
-        if (!(showingRainbow)) {
+        if (!(showingFullRainbow)) {
             strip.showRainbow(1, 360)
-            showingRainbow = true
+            showingFullRainbow = true
         } else {
             strip.rotate(4)
             strip.show()
